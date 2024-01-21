01edb2a0dbbeab52edb5a08766a03e8d1705353d
huynamboz <teahuynam2003@gmail.com>
2024-01-22 00:42:54 +0700

    fix: fix undefined onScrolling

--- dist/vue-drag-scroller.es.js
+++ dist/vue-drag-scroller.es.js
@@ -1,46 +1,46 @@
-const v = "drag-scroller-disable", S = /* @__PURE__ */ (() => {
-  const n = /* @__PURE__ */ new WeakMap();
+const S = "drag-scroller-disable", L = /* @__PURE__ */ (() => {
+  const l = /* @__PURE__ */ new WeakMap();
   return {
     mounted(r, e) {
       let a = !1;
-      const { onlyX: l, onlyY: p, disablechild: d } = e.modifiers, i = (t) => {
-        if (d)
+      const { onlyX: s, onlyY: d, disablechild: v } = e.modifiers, i = (t) => {
+        if (v)
           return t === r;
         for (; t && t.parentNode; ) {
-          if (t && (t != null && t.hasAttribute(v)))
+          if (t && (t != null && t.hasAttribute(S)))
             return !1;
           if (t === r)
             return !0;
           t = t.parentNode;
         }
         return !1;
-      }, s = (t) => {
+      }, n = (t) => {
         var o;
         a = i(t.target), a && ((o = e.value) != null && o.startScroll) && typeof e.value.startScroll == "function" && e.value.startScroll(t);
       }, c = (t) => {
         var o;
-        a && ((o = e.value) != null && o.endScroll) && typeof e.value.endScroll == "function" && e.value.endScroll(t), a = !1;
+        a && e.value && ((o = e.value) != null && o.endScroll) && typeof e.value.endScroll == "function" && e.value.endScroll(t), a = !1;
       }, u = (t) => {
-        var f;
+        var f, p;
         if (!a)
           return !1;
-        e.value.onScrolling && typeof e.value.onScrolling == "function" && e.value.onScrolling(t), t.stopPropagation && t.stopPropagation(), t.preventDefault && t.preventDefault(), t.cancelBubble = !0, t.returnValue = !1;
-        const o = ((f = e.value) == null ? void 0 : f.speed) || 1;
-        l ? r.scrollLeft -= t.movementX * o : (p || (r.scrollLeft -= t.movementX * o), r.scrollTop -= t.movementY * o);
+        e.value && ((f = e.value) != null && f.onScrolling) && typeof e.value.onScrolling == "function" && e.value.onScrolling(t), t.stopPropagation && t.stopPropagation(), t.preventDefault && t.preventDefault(), t.cancelBubble = !0, t.returnValue = !1;
+        const o = ((p = e.value) == null ? void 0 : p.speed) || 1;
+        s ? r.scrollLeft -= t.movementX * o : (d || (r.scrollLeft -= t.movementX * o), r.scrollTop -= t.movementY * o);
       };
-      n.set(r, { dragStart: s, dragEnd: c, drag: u }), r.addEventListener("pointerdown", s), addEventListener("pointerup", c), addEventListener("pointermove", u);
+      l.set(r, { dragStart: n, dragEnd: c, drag: u }), r.addEventListener("pointerdown", n), addEventListener("pointerup", c), addEventListener("pointermove", u);
     },
     unmounted(r) {
-      const { dragStart: e, dragEnd: a, drag: l } = n.get(r);
-      r.removeEventListener("pointerdown", e), removeEventListener("pointerup", a), removeEventListener("pointermove", l);
+      const { dragStart: e, dragEnd: a, drag: s } = l.get(r);
+      r.removeEventListener("pointerdown", e), removeEventListener("pointerup", a), removeEventListener("pointermove", s);
     }
   };
-})(), L = {
-  install(n) {
-    n.directive("drag-scroller", S);
+})(), E = {
+  install(l) {
+    l.directive("drag-scroller", L);
   }
 };
 export {
-  L as default,
-  S as dragScroller
+  E as default,
+  L as dragScroller
 };
