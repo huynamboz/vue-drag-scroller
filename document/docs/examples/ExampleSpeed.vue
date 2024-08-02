31c25a0025b6c6dd23b6ecbe0c63147ae8a2c672
huynamboz <teahuynam2003@gmail.com>
2024-08-02 17:01:09 +0700

    fix: prevent select

--- document/docs/examples/ExampleSpeed.vue
+++ document/docs/examples/ExampleSpeed.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { dragScroller as vDragScroller } from  "vue-drag-scroller"
+import { dragScroller as vDragScroller } from  "../../../src/index"
 
 const options = {
   speed: 0.5 // default is 1
